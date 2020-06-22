d66ad1b764c7251a8069dc5b406d58decf2196be
Souvik Dutta <64468652+souvikdutta7012@users.noreply.github.com>
2020-06-22 17:16:12 +0530

    v1

--- task6.groovy
+++ task6.groovy
@@ -23,61 +23,40 @@ job("job1") {
 
 
 job("job2") {
-
-
-  description("Deploying job")
-    
+  description("Deploying job") 
   triggers {
     upstream {
       upstreamProjects("job1")
       threshold("SUCCESS")
     }  
   }
-
   steps {
-      shell("if sudo kubectl get pods | grep mypod; then sudo kubectl delete pods --all; fi;if sudo kubectl get service | grep myservice ; then sudo kubectl delete service --all; fi;if sudo kubectl get pvc | grep mywebpvc; then sudo kubectl delete pvc --all; fi;kubectl create -f /root.devopstask6/mypvc.yml;kubectl create -f /root/devopstask6/deploy.yml; kubectl create -f /root/devopstask6/service.yml;kubectl get all;") 
-
+      shell("if sudo kubectl get pods | grep mypod; then sudo kubectl delete pods --all; fi;if sudo kubectl get service | grep myservice ; then sudo kubectl delete service --all; fi;if sudo kubectl get pvc | grep mywebpvc; then sudo kubectl delete pvc --all; fi;sudo kubectl create -f /root/devopstask6/mypvc.yml;sudo kubectl create -f /root/devopstask6/deploy.yml;sudo kubectl create -f /root/devopstask6/service.yml;sudo kubectl get all;") 
   }
 }
-
 
 
 
 job("job3") {
-  
   description("Monitoring Job")
-
-
   triggers {
      scm("* * * * *")
    }
-
-
   steps {
-    
+    shell("sudo cd /root/devopstask6")
     shell('export status=$(curl -siw "%{http_code}" -o /dev/null 192.168.99.100:30000); if [ $status -eq 200 ]; then exit 0; else python3 sendmail.py; exit 1; fi')
-
   }
 }
 
 
-
-
-
 job("job4") {
-
-
   description("Redeploying Job")
-
-
   triggers {
     upstream {
       upstreamProjects("job3")
       threshold("FAILURE")
     }
   }
-  
-  
   publishers {
     postBuildScripts {
       steps {
